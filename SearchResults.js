5729fa26bb01e68944960ad701f44a3a4b87152c
Joseph Brady <springyatomoutlook.com>
2022-04-07 10:15:20 +0100

    Adding and removing Styles for element length

---
+++ SearchResults.js
@@ -0,0 +1,11 @@
+function toggleText() {
+    var x = document.getElementById("test");
+    if (x.style.display === "none") {
+      //Function displays the text as grid when it is shown to fit the layout
+      x.style.display = "grid";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
